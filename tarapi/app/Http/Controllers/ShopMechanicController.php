ab202fc23c196d2f3b5e00b5ed840cf9b0900c19
pandesalNaSunog <floresjem8@gmail.com>
2022-09-13 12:40:23 +0800

    modified booking feature

--- tarapi/app/Http/Controllers/ShopMechanicController.php
+++ tarapi/app/Http/Controllers/ShopMechanicController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Booking;
+use Laravel\Sanctum;
+use Laravel\Sanctum\PersonalAccessToken;
 class ShopMechanicController extends Controller
 {
     public function getMechanics(){
@@ -15,12 +17,19 @@ class ShopMechanicController extends Controller
 
     public function book(Request $request){
         $request->validate([
-            'customer_id' => 'required',
             'shop_mechanic_id' => 'required',
             'lat' => 'required',
             'long' => 'required'
         ]);
-        $booking = Booking::create($request->all());
+
+        $token = PersonalAccessToken::findToken($request->bearerToken());
+        $id = $token->tokenable->id;
+        $booking = Booking::create([
+            'customer_id' => $id,
+            'shop_mechanic_id' => $request['shope_mechanic_id'],
+            'lat' => $request['lat'],
+            'long' => $request['long'],
+        ]);
 
         return response([
             'message' => 'successfully booked'
