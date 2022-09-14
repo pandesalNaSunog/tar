119d92d52bf6ffe17db667eb52fdacfc62109834
pandesalNaSunog <floresjem8@gmail.com>
2022-09-14 13:44:53 +0800

    latest

--- idle.php
+++ idle.php
@@ -1,6 +1,6 @@
 <?php
-    include('admin/php/connection.php');
-    $con = connect();
+    
+    $con = new mysqli ("localhost","u568496919_tar","TarPassword11","u568496919_tar_db");
     $query = "UPDATE users SET status = 'idle'";
     $con->query($query) or die($con->error);
     echo 'ok';
