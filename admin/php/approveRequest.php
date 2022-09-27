2aa81f83b69342f5e4ba63a520d642f64f2c8b57
pandesalNaSunog <floresjem8@gmail.com>
2022-09-27 22:49:59 +0800

    latest

--- admin/php/approveRequest.php
+++ admin/php/approveRequest.php
@@ -1,4 +1,9 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+    require 'phpmailer/src/SMTP.php';
     if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
         date_default_timezone_set('Asia/Manila');
         $today = date('Y-m-d H:i:s');
@@ -16,6 +21,14 @@
                 if($num_rows == '1'){
                     $query = "UPDATE `users` SET `approval_status`='Approved',`updated_at`='$today' WHERE id ='$requestId'";
                     $con->query($query) or die($con->error);
+
+                    $query = "SELECT * FROM users WHERE id = '$requestId'";
+                    $user = $con->query($query) or die($con->error);
+                    $userRow = $user->fetch_assoc();
+
+                    $email = $userRow['email'];
+
+                    sendMail($email);
                     echo 'success';
                 }else{
                     echo 'data missing';
@@ -23,5 +36,27 @@
             }
     }else{
         echo header('HTTP/1.1 403 Forbidden');
+    }
+
+    function sendMail($email){
+        
+        $mail = new PHPMailer(true);
+
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'tapandrepair@gmail.com';
+        $mail->Password = 'jamxdnzynricpvlr';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        $mail->setFrom('tapandrepair@gmail.com', 'Tap And Repair');
+        $mail->addAddress($email);
+        $mail->isHTML(true);
+
+        $mail->Subject = 'Approved';
+        $mail->Body = 'Your account has been approved by the administrator. You can now proceed to log in.';
+        $mail->send();
     }
 ?>
