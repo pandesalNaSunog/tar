e2f359301e8acb1814f1a628a0d70280d4108662
pandesalNaSunog <floresjem8@gmail.com>
2022-09-27 16:21:48 +0800

    latest

--- admin/php/Mcounter.php
+++ admin/php/Mcounter.php
@@ -2,7 +2,7 @@
     if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
         include('connection.php');
         $con = connect();
-        $query = "SELECT count(id) AS total FROM users WHERE user_type ='Mechanic' AND approval_status = 'pending'";
+        $query = "SELECT count(id) AS total FROM users WHERE user_type ='Mechanic' AND approval_status = 'pending' AND verified = 'yes'";
         $result = mysqli_query($con,$query);
         $values = mysqli_fetch_assoc($result);
         $num_rows = $values['total'];
