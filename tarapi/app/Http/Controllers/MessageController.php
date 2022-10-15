ee3cc364b6d567cca425eab59fa9bf47f4e41106
pandesalNaSunog <floresjem8@gmail.com>
2022-10-15 19:26:48 +0800

    latest

--- tarapi/app/Http/Controllers/MessageController.php
+++ tarapi/app/Http/Controllers/MessageController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Laravel\Sanctum;
-use App\Modesl\Message;
+use App\Models\Message;
 use Laravel\Sanctum\PersonalAccessToken;
 class MessageController extends Controller
 {
