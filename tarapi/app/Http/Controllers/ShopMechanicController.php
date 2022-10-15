fc5a5dcadfbd607678568c47ef59bb1a6ce1b166
pandesalNaSunog <floresjem8@gmail.com>
2022-10-15 14:10:26 +0800

    latest

--- tarapi/app/Http/Controllers/ShopMechanicController.php
+++ tarapi/app/Http/Controllers/ShopMechanicController.php
@@ -81,7 +81,7 @@ class ShopMechanicController extends Controller
             $distances[] = $responseItem['distance'];
         }
 
-        array_multisort($distances, SORT_DESC, $response);
+        array_multisort($distances, SORT_ASC, $response);
 
         return response($response, 200);
     }
