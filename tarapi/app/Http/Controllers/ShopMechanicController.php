dae9e2042b505783a8b9300fa7a4b09722c02599
pandesalNaSunog <floresjem8@gmail.com>
2022-10-17 14:31:59 +0800

    latest

--- tarapi/app/Http/Controllers/ShopMechanicController.php
+++ tarapi/app/Http/Controllers/ShopMechanicController.php
@@ -142,7 +142,9 @@ class ShopMechanicController extends Controller
         $token = PersonalAccessToken::findToken($request->bearerToken());
         $id = $token->tokenable->id;
 
-        $bookings = Booking::where('customer_id', $id)->first();
+        $bookings = Booking::where('customer_id', $id)->where(function($query){
+            $query->where('status', 'accepted')->orWhere('status', 'pending',)->first();
+        });
         if($bookings){
             return response([
                 'message' => 'you are currently booked to a mechanic/shop'
@@ -383,7 +385,9 @@ class ShopMechanicController extends Controller
         }
         $token = PersonalAccessToken::findToken($request->bearerToken());
         $id = $token->tokenable->id;
-        $bookings = Booking::where('customer_id', $id)->first();
+        $bookings = Booking::where('customer_id', $id)->where(function($query){
+            $query->where('status', 'accepted')->orWhere('status', 'pending',)->first();
+        });
 
         if($bookings){
             return response([
