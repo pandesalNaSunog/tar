1b7b4cbd216743c7f175db1c9b871b1310411a38
pandesalNaSunog <floresjem8@gmail.com>
2022-10-20 12:50:23 +0800

    latest

--- admin/php/createAdmin.php
+++ admin/php/createAdmin.php
@@ -2,8 +2,8 @@
     //if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
         date_default_timezone_set('Asia/Manila');
         $today = date('Y-m-d H:i:s');
-        $con = new mysqli ("localhost","u568496919_tar","TarPassword11","u568496919_tar_db");
-
+        //$con = new mysqli ("localhost","u568496919_tar","TarPassword11","u568496919_tar_db");
+        $con = new mysqli ("localhost","root","","tar_db");
         $query = "SELECT * FROM users WHERE user_type = 'admin'";
 
         $user = $con->query($query) or die($con->error);
