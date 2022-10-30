1d42958d44b85a0bd3d23619eb1d12c3af19363c
pandesalNaSunog <floresjem8@gmail.com>
2022-10-30 23:45:47 +0800

    latest

--- tarapi/database/migrations/2022_10_30_230638_create_activity_logs_table.php
+++ tarapi/database/migrations/2022_10_30_230638_create_activity_logs_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('activity_logs', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->integer('activity');
+            $table->string('activity');
             $table->timestamps();
         });
     }
